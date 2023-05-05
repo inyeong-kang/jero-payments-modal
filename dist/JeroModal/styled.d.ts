cf63e523553f7066baedb2134a556dd3ac5b61be
inyeong-kang <tomatorkd@naver.com>
2023-05-06 08:06:28 +0900

    delete: dist folder

--- dist/JeroModal/styled.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const _Backdrop: import("styled-components").StyledComponent<"div", any, {}, never>;
-export declare const _Container: import("styled-components").StyledComponent<"div", any, {}, never>;
--- dist/index.js
+++
@@ -1,1 +0,0 @@
-export { default as JeroModal } from './JeroModal';
