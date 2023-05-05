2546f73d447cde1c4090347a23c0a5e425668ce4
inyeong-kang <tomatorkd@naver.com>
2023-05-06 08:45:27 +0900

    change version

--- dist/JeroModal/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch } from 'react';
-interface ModalProps {
-    onModalClose: Dispatch<React.SetStateAction<boolean>>;
-    children: () => JSX.Element;
-}
-export default function JeroModal({ onModalClose, children }: ModalProps): JSX.Element;
-export {};
